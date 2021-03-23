aab4fc2c9724e0d7dadae54cedb4d6351c419092
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2021-03-23 14:18:20 +0200

    finish_ruby_on_rails_terminale_linux

---
+++ Ruby_on_rails/Linux_terminal/tree_files.js
@@ -0,0 +1,25 @@
+[{"type":"directory","name": ".","contents":[
+    {"type":"directory","name":"React_roadmap","contents":[
+      {"type":"directory","name":"1_HTML","contents":[
+        {"type":"directory","name":"2_Semantic_HTML","contents":[
+          {"type":"file","name":"1.txt"}
+        ]}
+      ]}
+    ]},
+    {"type":"file","name":"README.md"},
+    {"type":"directory","name":"Ruby_on_rails","contents":[
+      {"type":"file","name":"1.txt"},
+      {"type":"file","name":"2.txt"},
+      {"type":"directory","name":"Linux_terminal","contents":[
+        {"type":"file","name":"Link.html"},
+        {"type":"directory","name":"src","contents":[
+          {"type":"directory","name":"img","contents":[
+            {"type":"file","name":"Screenshot from 2021-03-22 11-41-22.png"},
+            {"type":"file","name":"Screenshot from 2021-03-22 13-31-03.png"}
+          ]}
+        ]}
+      ]}
+    ]}
+  ]},
+  {"type":"report","directories":7,"files":7}
+]
